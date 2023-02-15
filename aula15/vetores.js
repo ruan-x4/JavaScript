440e6c7b2977cadb834eb8a45ff5ebbaddcd1e75
Ruan Lima <124160424+ruan-x4@users.noreply.github.com>
2023-02-15 17:34:15 -0300

    funções

---
+++ aula15/vetores.js
@@ -0,0 +1,9 @@
+let num = [5, 8, 2, 9]
+
+num.sort()
+num.push(1)
+console.log(num)
+console.log(`O vetor tem ${num.length} posições`)
+console.log(num[0])
+let pos = num.indexOf(8)
+console.log(`O valor 8 está na posição ${pos}`)
