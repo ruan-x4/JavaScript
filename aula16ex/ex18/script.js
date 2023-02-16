7b7b2aa422c97328e3c276fa627f23d87669d10e
Ruan Lima <124160424+ruan-x4@users.noreply.github.com>
2023-02-16 10:32:17 -0300

    Analisador de número ex018

--- aula16ex/ex18/script.js
+++ aula16ex/ex18/script.js
@@ -21,12 +21,42 @@ function islist(n, l) {
 
 function adicionar() {
     if (isNumber(num.value) && !islist(num.value, valores)) {
-        
+        valores.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `Valor ${num.value} adicionado.`
+        lista.appendChild(item)
+        res.innerHTML = ''
     } else {
         alert('Valor inválido ou já encontrado na lista')
     }
+    num.value = ''
+    num.focus()
 }
 
 function finalizar() {
-
+    if (valores.length == 0) {
+        alert('Adiciones valores antes de finalizar!')
+    } else {
+        let total = valores.length
+        let maior = valores[0]
+        let menor = valores[0]
+        let soma = 0
+        let media = 0
+        for (let pos in valores) {
+            soma += valores[pos]
+            if (valores[pos] > maior) {
+                maior = valores[pos]
+            }
+            if (valores[pos] < menor) {
+                menor = valores[pos]
+            }
+        }
+        media = soma / total
+        res.innerHTML = ''
+        res.innerHTML += `<P>Ao todo, temos ${total} números cadastrados.</P>`
+        res.innerHTML += `<p>O maior valor informado foi ${maior}</p>`
+        res.innerHTML += `<p>O menor valor informado foi ${menor}</p>`
+        res.innerHTML += `<p>A soma de todos valores é ${soma} </p>`
+        res.innerHTML += `<p>A média dos valores é ${media} </p>`
+    }
 }
